e19e371e6d928bfde876e782ecaf2c2d670eabb7
Akakileqvinadze <akakileqvinadze@yahoo.com>
2025-06-16 13:26:32 +0400

    Add files via upload

---
+++ C++_Amocanebi_2/C++_amocanebi_2_5/C++_amocanebi_2_5/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cmath> // <- ჯობს <cmath> გამოიყენო
+
+int main()
+{
+    double x;
+    std::cin >> x;
+    std::cout << "x = " << x << std::endl;
+
+    return 0;
+}
+
